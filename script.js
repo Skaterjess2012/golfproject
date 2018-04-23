363a1a4b54907b19ff348b8ae498d2122519eafc
Jesse Garcia <skaterjess2012@gmail.com>
2018-04-23 08:53:27 -0600

    Added the ability to have multiple golf courses

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-let golfholes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 let numPlayers = 4;
 let course;
+let allcourses;
 
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-       course = JSON.parse(this.responseText);
-       console.log(course);
-       createCard();
+       allcourses = JSON.parse(this.responseText);
+       for(let i = 0; i < allcourses.courses.length; i++){
+            let courseid = allcourses.courses[i].id;
+            $('.courseDropdown').append('<option value="'+ courseid +'">'+ allcourses.courses[i].name +'</option>');
+       }
       }
     };
-    xhttp.open("GET", "holes.txt", true);
+    xhttp.open("GET", "https://uxcobra.com/golfapi/courses.txt", true);
     xhttp.send();
   }
 
 
 function createCard(){
-    for(let i = 0; i < course.holes.length; i++){
-        $('.right').append('<div id="col' + i + '" class="column"><div class="cheader"><span class="holeName">' + course.holes[i].name +'</span><span class="yards">' +  + '</span></div></div>');
-        console.log(course.holes[i].name);
-        console.log(course.holes[i]);
+    console.log(course);
+    for(let i = 0; i < course.data.holes.length; i++){
+        $('.right').append('<div id="col' + i + '" class="column"><div class="cheader"><span class="holeName">' + course.data.holes[i].hole +'</span><span class="yards">' + course.data.holes[i].teeBoxes[0].yards + ' YRD</span></div></div>');
     }
     fillCard();
 }
 function fillCard(){
     for (let p = 0; p < numPlayers; p++){
-        for(let h = 0; h < course.holes.length; h++){
+        for(let h = 0; h < course.data.holes.length; h++){
             $('#col' + h).append('<input id="p'+ p +' h'+ h +'" class="holeInput" type="text"></input>');
         }
     }
@@ -34,8 +35,22 @@ function fillCard(){
 
 function loadPlayers(){
     for(let i = 0; i < numPlayers; i++){
-        $('.left').append('<div class="playerLabel ' + (i + 1) + '">PlayerLabel' + (i + 1) + '</div>');
+        $('.playerContainer').append('<div class="playerLabel ' + (i + 1) + '">PlayerLabel' + (i + 1) + '</div>');
     }
 }
+function getCourse(courseid){
+    console.log(courseid);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       course = JSON.parse(this.responseText);
+       $('.right').empty();
+       $('.playerContainer').empty();
+       loadPlayers();
+       createCard();
+      }
+    };
+    xhttp.open("GET", 'https://uxcobra.com/golfapi/course'+ courseid +'.txt', true);
+    xhttp.send();
+}
 loadDoc();
-loadPlayers();
