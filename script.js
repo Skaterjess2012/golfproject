d1be7614713f44470708c2ffc48a51e53f24b42b
Jesse <skaterjess2012@gmail.com>
2018-05-01 17:52:52 -0600

    added totals

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let numPlayers = 4;
 let course;
-let allcourses;
+//let allcourses;
+let totalArray = [];
 
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
@@ -17,19 +18,64 @@ function loadDoc() {
     xhttp.send();
   }
 
+class ScoreCard{
+    constructor(){
+        this.course;
+        this.allcourses;
+        this.numPlayers;
+        this.loadDoc();
+    }
+    loadDoc(){
+        $.get('https://uxcobra.com/golfapi/courses.txt', function(data, status){
+            this.allcourses = JSON.parse(data);
+        });
+    }
+    getCourse(){
+        console.log(this.allcourses);
+        console.log(stuff);
+    }
+}
+//let stuff = new ScoreCard();
+
 
 function createCard(){
     console.log(course);
     for(let i = 0; i < course.data.holes.length; i++){
         $('.right').append('<div id="col' + i + '" class="column"><div class="cheader"><span class="holeName">' + course.data.holes[i].hole +'</span><span class="yards">' + course.data.holes[i].teeBoxes[0].yards + ' YRD</span></div></div>');
     }
+    $('.right').append('<div class="In"><span class="tHeader">In</span></div>');
+    $('.right').append('<div class="Out"><span class="tHeader">Out</span></div>');
     fillCard();
 }
-function fillCard(){
-    for (let p = 0; p < numPlayers; p++){
-        for(let h = 0; h < course.data.holes.length; h++){
-            $('#col' + h).append('<input id="p'+ p +' h'+ h +'" class="holeInput" type="text"></input>');
+$(document).on('keyup', '.holeInput', function(e){
+    let item = $(this);
+    let cord = {x:item.attr('x'), y:item.attr('y')};
+    
+    if(item.val() % 1 !== 0) {
+        item.val('');
+        item.css('background: red');
+    } else {
+        item.css('background: transparent');
+        if(item.val() !== "") {
+            curItemArrayObj.val = Number(item.val());
         }
+    }
+});
+
+
+function fillCard(){
+    for (let p = 0; p < numPlayers; p++){//how many rows
+        let curRow = [];
+        for(let h = 0; h < course.data.holes.length; h++){//how many columns
+            let curCol = {};
+            $('#col' + h).append('<input x="'+ h +'" y="'+ p +'" id="p'+ p +' h' + h + '" class="holeInput" type="text"></input>');
+            curCol.val = 0;
+            curRow.push(curCol);
+        }
+        $('.In').append('<div id="total'+ p +'" class="totalBox">0</div>');
+        $('.Out').append('<div id="total'+ p +'" class="totalBox">0</div>');
+        totalArray.push(curRow);
+        
     }
 }
 
